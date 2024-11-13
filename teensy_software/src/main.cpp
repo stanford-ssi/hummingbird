d4ae76fb4bc8598539647894b601e7280eb96dec
ls1031 <125716107+ls1031@users.noreply.github.com>
2024-11-12 20:56:51 -0800

    Writes to SD card (badly tho:) )

--- teensy_software/src/main.cpp
+++ teensy_software/src/main.cpp
@@ -12,6 +12,7 @@
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_Sensor.h>
 #include "Wire.h"
+#include <SD.h>
 
 // analog inputs: 
 // PT: pressure transducer
@@ -20,25 +21,37 @@
 #define LOAD_IN_1 A10
 #define LOAD_IN_2 A11
 
+    char buffer[50];
 // digital inputs via I2C: BME680 (can read temp, pressure, humidity, gas)
 // Note from Sam: I was too lazy (and don't have time) to configure the I2C setup from the ground up.
 // It can be done, but will need someone with substantial serial communication knowledge and lots of time on their hands.
 Adafruit_BME680 bme;    // initializes I2C object
-
+File myFile;
+const int chipSelect = BUILTIN_SDCARD;
 void setup() {
     // Teensy 4.1 default baud rate is 9600, or 6 Mbits/s
     Serial.begin(9600);
+    
+    Serial.println("initialiing SD card");
+    if (!SD.begin(chipSelect)){
 
+
+        Serial.println("initialization failed");
+        return;
+    }
+    Serial.println("initialization done");
+    
     // configure pin inputs/outputs.
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(PT_INPUT, INPUT);
     pinMode(LOAD_IN_1, INPUT);
     pinMode(LOAD_IN_2, INPUT);
+    myFile = SD.open("sensorData.txt", FILE_WRITE);
 
     // begin I2C object. See Adafruit.BME680.h for interface details
     bme.begin();
 }
-
+int count = 0;
 // the loop function runs over and over again forever
 void loop() {
     // uncomment each section when ready to test
@@ -71,12 +84,16 @@ void loop() {
     Serial.println(diff);
     */
     
-    /*
+    
+
     // I2C reading from BME680
     float temp = bme.readTemperature();
     Serial.print("Temp: ");
     Serial.println(temp);
-    */
+    snprintf(buffer, sizeof(buffer), "The value of heat is: %.2f", temp);
+    myFile.write(buffer);
+    
+    count++;
 
     /*
     // RFM95W LoRa Radio is included in seperate scripts:
@@ -86,5 +103,8 @@ void loop() {
     
 
     delay(50);    // delay 50ms
+    if( count > 5) {
 
+        myFile.close();
+    }
 }
