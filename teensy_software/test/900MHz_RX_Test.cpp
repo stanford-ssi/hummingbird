5765072499d6d304602787c76e91ce80523a609f
ls1031 <125716107+ls1031@users.noreply.github.com>
2024-11-17 15:22:00 -0800

    add sd card writing to radio receviing

--- teensy_software/test/900MHz_RX_Test.cpp
+++ teensy_software/test/900MHz_RX_Test.cpp
@@ -1,11 +1,3 @@
-// Feather9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Feather9x_TX
-
 #include "Arduino.h"
 #include <Adafruit_BME680.h>
 #include <Adafruit_I2CDevice.h>
@@ -22,19 +14,29 @@
 
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
-
+const int chipSelect = BUILTIN_SDCARD;
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
-
+File myFile;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
   Serial.begin(9600);
-  while (!Serial) delay(1);
-  delay(100);
 
+
+  while (!Serial) delay(1);
+      Serial.println("initialiing SD card");
+    if (!SD.begin(chipSelect)){
+
+
+        Serial.println("initialization failed");
+        return;
+    }
+    Serial.println("initialization done");
+  delay(100);
+     myFile = SD.open("sensorData.txt", FILE_WRITE);
   Serial.println("Feather LoRa RX Test!");
 
   // manual reset
@@ -75,7 +77,15 @@ void loop() {
       digitalWrite(LED_BUILTIN, HIGH);
       RH_RF95::printBuffer("Received: ", buf, len);
       Serial.print("Got: ");
+
       Serial.println((char*)buf);
+
+
+    char buffer1[140];
+    snprintf(buffer1, sizeof(buffer1), "The value of the received message is: %s\n", (char*)buf);
+     myFile.write(buffer1);
+    myFile.flush();
+
        Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
 
