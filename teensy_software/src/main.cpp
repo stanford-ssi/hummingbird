9d0f9042768abe62a80b9e1ed3bd26c1de5a7963
Sam Chen <samchen2@stanford.edu>
2024-11-19 21:15:40 -0800

    Load cell code

--- teensy_software/src/main.cpp
+++ teensy_software/src/main.cpp
@@ -11,6 +11,7 @@
 #include <Adafruit_BME680.h>
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_Sensor.h>
+#include <Adafruit_HX711.h>
 #include "Wire.h"
 #include <SD.h>
 
@@ -27,6 +28,13 @@ char buffer[50];
 // It can be done, but will need someone with substantial serial communication knowledge and lots of time on their hands.
 Adafruit_BME680 bme;    // initializes I2C object
 File myFile;
+
+// Define the pins for the HX711 communication (load cell)
+const uint8_t DATA_PIN = 18;  // Can use any pins!
+const uint8_t CLOCK_PIN = 19; // Can use any pins!
+
+Adafruit_HX711 hx711(DATA_PIN, CLOCK_PIN);
+
 const int chipSelect = BUILTIN_SDCARD;
 void setup() {
     // Teensy 4.1 default baud rate is 9600, or 6 Mbits/s
@@ -50,6 +58,18 @@ void setup() {
 
     // begin I2C object. See Adafruit.BME680.h for interface details
     bme.begin();
+
+      // Initialize the HX711
+    hx711.begin();
+
+    // read and toss 3 values each
+    Serial.println("Tareing....");
+    for (uint8_t t=0; t<3; t++) {
+        hx711.tareA(hx711.readChannelRaw(CHAN_A_GAIN_128));
+        hx711.tareA(hx711.readChannelRaw(CHAN_A_GAIN_128));
+        hx711.tareB(hx711.readChannelRaw(CHAN_B_GAIN_32));
+        hx711.tareB(hx711.readChannelRaw(CHAN_B_GAIN_32));
+    }
 }
 
 // the loop function runs over and over again forever
@@ -62,15 +82,15 @@ void loop() {
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
     delay(250);                      // wait for a second
 
-    /*
+    
     // pressure transducer outputs
     int pt = analogRead(PT_INPUT);  // should output a number between 0 and 1023.
                                     // theoretically translates to 0V - 3.3V.
     Serial.print("PT input: ");
     Serial.println(pt);
-    */
     
-    /*
+    
+    
     // load cell
     // the load cell is basically a wheatstone bridge and the force reading can be measured by
     // taking the difference between load_in_1 and load_in_2 (voltages measured on opposite ends of the bridge)
@@ -82,7 +102,7 @@ void loop() {
     Serial.println(load_in_2);
     Serial.print("Diff: ");
     Serial.println(diff);
-    */
+    
     
     
 
@@ -94,15 +114,26 @@ void loop() {
     myFile.write(buffer);
     
 
-    /*
+    
     // RFM95W LoRa Radio is included in seperate scripts:
-    900MHz_TX_Test.cpp
-    900MHz_RX_Test.cpp
-    */
+    // 900MHz_TX_Test.cpp
+    // 900MHz_RX_Test.cpp
+    
     
 
     delay(50);    // delay 50ms
-    myFile.flush();
+    // myFile.flush();
+
+    // Read from Channel A with Gain 128, can also try CHAN_A_GAIN_64 or CHAN_B_GAIN_32
+    // since the read is blocking this will not be more than 10 or 80 SPS (L or H switch)
+    int32_t weightA128 = hx711.readChannelBlocking(CHAN_A_GAIN_128);
+    Serial.print("Channel A (Gain 128): ");
+    Serial.println(weightA128);
+
+    // Read from Channel A with Gain 128, can also try CHAN_A_GAIN_64 or CHAN_B_GAIN_32
+    int32_t weightB32 = hx711.readChannelBlocking(CHAN_B_GAIN_32);
+    Serial.print("Channel B (Gain 32): ");
+    Serial.println(weightB32);
     // if( count  5) {
 
     //     myFile.close();
